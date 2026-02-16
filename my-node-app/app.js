5f2c1803594a81fbf384b01770a2379a3572b2bc
mr-tech9580 <megha.official9580.com>
2026-02-16 15:42:57 +0530

    new

--- my-node-app/app.js
+++ my-node-app/app.js
@@ -1,15 +1,62 @@
-const express = require("express");
-const app = express();
+// const express = require("express");
+// const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello Express!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello Express!");
+// });
 
-app.get("/about", (req, res) => {
-  res.send("This is About Page");
-});
+// app.get("/about", (req, res) => {
+//   res.send("This is About Page");
+// });
+
+// app.listen(3000, () => {
+//   console.log("Server running on port 3000");
+// });
+  
+
+ setTimeout(()=>{
+  console.log("1")
+  setTimeout(()=>{
+    console.log("1")
+    setTimeout(()=>{
+
+    })
+  })
+})
+
+const fs = require("fs");
+fs.readFile("file.txt","utf-8",function(err,data){
+  if(err){
+    console.log(data)
+  }
+  else{
+    console.log(err)
+  }
+  console.log(data)
+})
 
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+
+const nayapromise = new Promise((resolve,reject)=>{
+  let deliver = true;
+  if(deliver){
+    resolve("order deliver ho gaya")
+  }
+  else{
+    reject("ni hua deliver")
+  }
+})
+nayapromise
+.then(()=>console.log("ye hua resolve"))
+.catch(()=> console.log("ye hua resolve")) 
+
+
+
+const fs = require("fs")
+fs.promises.readFile("file.txt","utf-8")
+.then((data) => console.log(data))
+.catch((err)=> console.log(err.message))
+
+
+
+
